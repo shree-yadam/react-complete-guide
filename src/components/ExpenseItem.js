f9b678735a4989262ae5b6a6add3c11c44cb200f
Shree Devi Yadam <shree.yadam@gmail.com>
2022-09-29 23:13:20 -0700

    Add CSS styling to ExpenseItem component

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -1,12 +1,14 @@
+import './ExpenseItem.css'
+
 function ExpenseItem() {
   return (
-    <>
+    <div className="expense-item">
       <div>Date</div>
-      <div>
+      <div className="expense-item__description">
         <h2>Title</h2>
-        <div>Amount</div>
+        <div className="expense-item__price">Amount</div>
       </div>
-    </>
+    </div>
   );
 }
 
