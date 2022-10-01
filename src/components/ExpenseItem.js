9c65523442423463f1375100887b305adc087149
Shree Devi Yadam <shree.yadam@gmail.com>
2022-09-30 22:13:40 -0700

    Format Date

--- src/components/ExpenseItem.js
+++ src/components/ExpenseItem.js
@@ -2,10 +2,16 @@ import './ExpenseItem.css'
 
 function ExpenseItem(props) {
   const{title, amount, date} = props;
-
+  const month = date && date.toLocaleDateString('en-US', { month: 'long'});
+  const day = date && date.toLocaleDateString('en-US', { day: '2-digit'});
+  const year = date && date.getFullYear();
   return (
     <div className="expense-item">
-      <div>{date && date.toLocaleDateString()}</div>
+      <div>
+        <div>{month}</div>
+        <div>{year}</div>
+        <div>{day}</div>
+      </div>
       <div className="expense-item__description">
         <h2>{title}</h2>
         <div className="expense-item__price">{amount}</div>
