db84e3148d16082da2d756ab16fb280d5157b56d
Shree Devi Yadam <shree.yadam@gmail.com>
2022-10-25 21:40:33 -0700

    Save selected filter year

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,13 +1,21 @@
 import ExpensesFilter from "./ExpensesFilter";
 import ExpenseItem from "./ExpenseItem";
 import "./Expenses.css";
+import { useState } from "react";
 
 function Expenses(props) {
+
+  const [filterYear, setFilterYear] = useState();
   const { expenses } = props;
+
+  function filterYearChangeHandler(year) {
+    setFilterYear(year);
+    console.log(`Setting year in Expenses ${year}`);
+  }
 
   return (
     <div>
-      <ExpensesFilter />
+      <ExpensesFilter onFilterYear={filterYearChangeHandler}/>
       <div className="expenses">
         {expenses.map((e) => (
           <ExpenseItem
--- src/components/Expenses/ExpensesFilter.js
+++ src/components/Expenses/ExpensesFilter.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 import './ExpensesFilter.css';
 
-const ExpensesFilter = () => {
+const ExpensesFilter = (props) => {
+
+  const [filterYear, setFilterYear] = useState('2022');
+
+  function filterYearChangeHandler(e) {
+    console.log(e.target.value);
+    setFilterYear(e.target.value);
+    props.onFilterYear(e.target.value);
+  }
+
   return (
     <div className='expenses-filter'>
       <div className='expenses-filter__control'>
         <label>Filter by year</label>
-        <select>
+        <select value={filterYear} onChange={filterYearChangeHandler}>
           <option value='2022'>2022</option>
           <option value='2021'>2021</option>
           <option value='2020'>2020</option>
