5aec610b2175603db61f05d8380737cd8db90084
Shree Devi Yadam <shree.yadam@gmail.com>
2022-10-25 21:28:09 -0700

    Add Expense Filter component

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,19 +1,23 @@
-import ExpenseItem from './ExpenseItem';
-import './Expenses.css';
+import ExpensesFilter from "./ExpensesFilter";
+import ExpenseItem from "./ExpenseItem";
+import "./Expenses.css";
 
 function Expenses(props) {
-  const {expenses} = props;
+  const { expenses } = props;
 
   return (
-    <div className="expenses">
-      {expenses.map((e) => (
-        <ExpenseItem
-          title={e.title}
-          amount={e.amount}
-          date={e.date}
-          key={e.id}
-        />
-      ))}
+    <div>
+      <ExpensesFilter />
+      <div className="expenses">
+        {expenses.map((e) => (
+          <ExpenseItem
+            title={e.title}
+            amount={e.amount}
+            date={e.date}
+            key={e.id}
+          />
+        ))}
+      </div>
     </div>
   );
 }
